a2e4eed2fa68da6291bc5ed60b286e364c2bd6ee
Jennifer Chang <jenchang@iastate.edu>
2016-12-25 13:22:57 -0600

    debugging

--- R/colsummarize.R
+++ R/colsummarize.R
@@ -2,14 +2,13 @@
 #'
 #' This function calculates the mean, sd and n of each column
 #' @param dfi data.frame containing only numerical values
-#' @keywords summary
+#' @keywords jcpackr
 #' @export
 #' @examples
 #' library("Sleuth3")
 #' data("case0202")
 #' colsummarize(case0202)
- 
-# Get mean, SD and N of each column
+
 colsummarize<-function(dfi){
   names(dfi)
   dfo<-data.frame(Group=names(dfi),Mean=0,SD=0,N=0)
